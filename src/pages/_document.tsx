75d1754e9b6a543ac96abd8a995f380e5f700b88
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-15 16:22:37 -0300

    Import Archivo font from google fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,7 @@ export default class Document extends NextDocument {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins&family=Roboto:wght@400;500;700&family=Ubuntu:wght@400;500;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins&family=Roboto:wght@400;500;700&family=Ubuntu:wght@400;500;700&Archivo:wght@700&display=swap"
             rel="stylesheet"
           />
         </Head>
