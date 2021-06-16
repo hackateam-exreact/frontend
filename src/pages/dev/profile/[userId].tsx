08dd86e44f73a2b3682584471083e0e84b5921a5
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-16 19:04:04 -0300

    Create and start shaping grid for authenticated user profile page

--- src/pages/dev/profile/[userId].tsx
+++ src/pages/dev/profile/[userId].tsx
@@ -1,9 +1,12 @@
+import { Box, Grid, GridItem, VStack } from '@chakra-ui/react'
 import { IUser, ProfileSummary } from 'components/Profile/ProfileSummary'
 
+import { Container } from 'components/Container'
 import { EditProfileBtn } from 'components/Profile/EditProfileBtn'
 import { EditProfileModal } from 'components/Modal/EditProfileModal'
 import { EditProfileProvider } from 'contexts/EditProfileContext'
 import Head from 'next/head'
+import { ProfileDescription } from 'components/Profile/ProfileDescription'
 
 export default function ProfilePage() {
   const user: IUser = {
@@ -14,6 +17,8 @@ export default function ProfilePage() {
     location: 'Balneário Camboriú',
     contact: '42988860098',
     status: 'Open',
+    about:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem maiores aspernatur quasi sed alias quo. Consequatur magnam provident iusto error rem dicta, esse dignissimos. Alias dolore dolor voluptatibus veniam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem maiores aspernatur quasi sed alias quo. Consequatur magnam provident iusto error rem dicta, esse dignissimos. Alias dolore dolor voluptatibus veniam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem maiores aspernatur quasi sed alias quo. Consequatur magnam provident iusto error rem dicta, esse dignissimos. Alias dolore dolor voluptatibus veniam aperiam.',
     created_at: String(Date.now()),
     updated_at: String(Date.now())
   }
@@ -24,11 +29,29 @@ export default function ProfilePage() {
         <title>Luis Filipe | Devspot</title>
       </Head>
 
-      <ProfileSummary user={user} />
-      <EditProfileProvider>
-        <EditProfileBtn user={user} />
-        <EditProfileModal />
-      </EditProfileProvider>
+      <Box h="6rem" mb="1rem" />
+
+      <Container>
+        <Grid
+          templateColumns="repeat(3, 1fr)"
+          gap="5rem"
+          maxW="1200px"
+          mx="auto"
+        >
+          <GridItem>
+            <VStack align="center" spacing="10">
+              <ProfileSummary user={user} />
+              <EditProfileProvider>
+                <EditProfileBtn user={user} />
+                <EditProfileModal />
+              </EditProfileProvider>
+            </VStack>
+          </GridItem>
+          <GridItem colSpan={2}>
+            <ProfileDescription description={user.about} />
+          </GridItem>
+        </Grid>
+      </Container>
     </>
   )
 }
