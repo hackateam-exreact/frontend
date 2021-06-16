a43e0fcd7a7f2254168183973850b275df37a030
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-16 12:02:30 -0300

    Create edit profile btn

---
+++ src/components/Profile/EditProfileBtn.tsx
@@ -0,0 +1,22 @@
+import { Button } from 'components/Button'
+import { FiEdit } from 'react-icons/fi'
+import { IUser } from './ProfileSummary'
+import { Icon } from '@chakra-ui/react'
+import { useEditProfile } from 'hooks/useEditProfile'
+
+interface EditProfileBtnProps {
+  user: IUser
+}
+
+export function EditProfileBtn({ user }: EditProfileBtnProps) {
+  const { handleEditUserProfile } = useEditProfile()
+
+  return (
+    <Button
+      leftIcon={<Icon as={FiEdit} />}
+      onClick={() => handleEditUserProfile(user)}
+    >
+      Editar perfil
+    </Button>
+  )
+}
