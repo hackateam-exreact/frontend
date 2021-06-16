b7f3c62bc8ee23b515e596949962e07c385021ac
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-16 11:57:20 -0300

    Create edit profile context and a hook to use it

---
+++ src/contexts/EditProfileContext.tsx
@@ -0,0 +1,38 @@
+import { Dispatch, SetStateAction } from 'react'
+import { ReactNode, createContext, useState } from 'react'
+import { UseDisclosureReturn, useDisclosure } from '@chakra-ui/react'
+
+import { IUser } from 'components/Profile/ProfileSummary'
+
+interface EditProfileContextData {
+  user: IUser
+  setUser: Dispatch<SetStateAction<IUser>>
+  disclosure: UseDisclosureReturn
+  handleEditUserProfile: (user: IUser) => void
+}
+
+interface EditProfileProviderProps {
+  children: ReactNode
+}
+
+export const EditProfileContext = createContext({} as EditProfileContextData)
+
+export function EditProfileProvider({ children }: EditProfileProviderProps) {
+  const [user, setUser] = useState<IUser>({} as IUser)
+
+  const disclosure = useDisclosure()
+
+  const handleEditUserProfile = (user: IUser) => {
+    setUser(user)
+
+    disclosure.onOpen()
+  }
+
+  return (
+    <EditProfileContext.Provider
+      value={{ user, setUser, disclosure, handleEditUserProfile }}
+    >
+      {children}
+    </EditProfileContext.Provider>
+  )
+}
