98b02c40f850d4568647a13047ef65a13ab63861
André <andrevictor50@gmail.com>
2021-06-15 21:34:12 -0300

    test: adicionando testes faltantes para o componente de Input

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
     '!src/**/*.stories.tsx',
     '!src/pages/**/*.tsx',
     '!src/styles/**/*.ts',
-    '!src/utils/apollo.ts',
+    '!src/utils/**',
     '!src/types/**/*.d.ts',
     '!src/components/**/mock.ts',
     '!src/theme/**'
