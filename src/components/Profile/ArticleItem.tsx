31472234951b8cb6242964358a439192d99cb830
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-16 21:05:39 -0300

    Create userBadge articleList and articleItem components

--- src/components/Profile/ArticleItem.tsx
+++ src/components/Profile/ArticleItem.tsx
@@ -1,16 +1,40 @@
-import { Flex, Heading, VStack } from '@chakra-ui/react'
+import { Flex, HStack, Icon, Text, VStack } from '@chakra-ui/react'
 
+import { FiClock } from 'react-icons/fi'
 import { IArticle } from 'interfaces/article'
+import { IUser } from 'interfaces/user'
+import { UserBadge } from './UserBadge'
 
 interface ArticleItemProps {
+  user: IUser
   article: IArticle
 }
 
-export function ArticleItem({ article }: ArticleItemProps) {
+export function ArticleItem({ user, article }: ArticleItemProps) {
   return (
-    <VStack spacing="5" p="3" bg="gray.800" borderRadius="md">
-      <Heading as="h3">{article.title}</Heading>
-      <Flex align="center" justify="space-between"></Flex>
+    <VStack
+      align="flex-start"
+      spacing="10"
+      p="3"
+      bg="gray.800"
+      borderRadius="md"
+      w="100%"
+    >
+      <Text fontSize="lg" fontWeight="bold">
+        {article.title}
+      </Text>
+      <Flex align="center" justify="space-between" w="100%">
+        <UserBadge user={user} />
+        <HStack>
+          <Icon as={FiClock} />
+          <Text fontSize="xs" color="gray.500">
+            5 minutos de leitura
+          </Text>
+        </HStack>
+        <Text fontSize="xs" color="gray.500">
+          16/06/2021
+        </Text>
+      </Flex>
     </VStack>
   )
 }
