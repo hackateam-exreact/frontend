f83a6874da1c5e31f20a46ca2a2653e710e0b339
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-17 17:00:28 -0300

    Create edit profile schema for yup

--- src/utils/yupSchemas.ts
+++ src/utils/yupSchemas.ts
@@ -11,3 +11,10 @@ export const signUpSchema = yup.object().shape({
   email: yup.string().email('Email inválido').required('Email obrigatório'),
   password: yup.string().required('Senha obrigatória')
 })
+
+export const editProfileSchema = yup.object().shape({
+  about: yup.string().required('Sobre obrigatório'),
+  username: yup.string().required('Username obrigatório'),
+  email: yup.string(),
+  phone: yup.string()
+})
