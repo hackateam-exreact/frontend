40e43b05e72bee82c95b08c16651f33830d4cb96
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-17 15:33:28 -0300

    move user template to its own file

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -7,6 +7,7 @@ import {
 } from 'react'
 
 import { IUser } from 'interfaces/user'
+import { userTemplate } from 'utils/userTemplate'
 
 interface AuthContextData {
   user: IUser
@@ -20,86 +21,7 @@ interface AuthProviderProps {
 export const AuthContext = createContext({} as AuthContextData)
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const [user, setUser] = useState<IUser>({
-    id: '123',
-    avatar: '/img/chakra-logo.png',
-    first_name: 'Luis Filipe',
-    last_name: 'Fernandes Almeida',
-    email: 'luisfilipe.faw@gmail.com',
-    location: 'Balneário Camboriú',
-    contact: '42988860098',
-    status: 'Open',
-    about:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem maiores aspernatur quasi sed alias quo. Consequatur magnam provident iusto error rem dicta, esse dignissimos. Alias dolore dolor voluptatibus veniam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem maiores aspernatur quasi sed alias quo. Consequatur magnam provident iusto error rem dicta, esse dignissimos. Alias dolore dolor voluptatibus veniam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem maiores aspernatur quasi sed alias quo. Consequatur magnam provident iusto error rem dicta, esse dignissimos. Alias dolore dolor voluptatibus veniam aperiam.',
-    articles: [
-      {
-        id: 'adasd',
-        title: 'Lorem Ipsum',
-        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sunt necessitatibus mollitia ipsa tempora. Odit, esse fuga nostrum tempore aliquid cum. Doloribus, minima quis eius perferendis consequuntur quia perspiciatis odit.',
-        created_at: String(Date.now()),
-        updated_at: String(Date.now())
-      },
-      {
-        id: 'adasd',
-        title: 'Lorem Ipsum',
-        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sunt necessitatibus mollitia ipsa tempora. Odit, esse fuga nostrum tempore aliquid cum. Doloribus, minima quis eius perferendis consequuntur quia perspiciatis odit.',
-        created_at: String(Date.now()),
-        updated_at: String(Date.now())
-      },
-      {
-        id: 'adasd',
-        title: 'Lorem Ipsum',
-        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sunt necessitatibus mollitia ipsa tempora. Odit, esse fuga nostrum tempore aliquid cum. Doloribus, minima quis eius perferendis consequuntur quia perspiciatis odit.',
-        created_at: String(Date.now()),
-        updated_at: String(Date.now())
-      },
-      {
-        id: 'adasd',
-        title: 'Lorem Ipsum',
-        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sunt necessitatibus mollitia ipsa tempora. Odit, esse fuga nostrum tempore aliquid cum. Doloribus, minima quis eius perferendis consequuntur quia perspiciatis odit.',
-        created_at: String(Date.now()),
-        updated_at: String(Date.now())
-      }
-    ],
-    projects: [
-      {
-        id: 'asdsada',
-        title: 'Lorem Ipsum',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sunt necessitatibus mollitia ipsa tempora. Odit, esse fuga nostrum tempore aliquid cum. Doloribus, minima quis eius perferendis consequuntur quia perspiciatis odit.'
-      },
-      {
-        id: 'asdsada',
-        title: 'Lorem Ipsum',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sunt necessitatibus mollitia ipsa tempora. Odit, esse fuga nostrum tempore aliquid cum. Doloribus, minima quis eius perferendis consequuntur quia perspiciatis odit.'
-      },
-      {
-        id: 'asdsada',
-        title: 'Lorem Ipsum',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sunt necessitatibus mollitia ipsa tempora. Odit, esse fuga nostrum tempore aliquid cum. Doloribus, minima quis eius perferendis consequuntur quia perspiciatis odit.'
-      },
-      {
-        id: 'asdsada',
-        title: 'Lorem Ipsum',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sunt necessitatibus mollitia ipsa tempora. Odit, esse fuga nostrum tempore aliquid cum. Doloribus, minima quis eius perferendis consequuntur quia perspiciatis odit.'
-      }
-    ],
-    techs: [
-      {
-        id: '213123',
-        thumbnail: '/img/react-logo.png',
-        title: 'React JS',
-        experience: '1-2 anos',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sunt necessitatibus mollitia ipsa tempora. Odit, esse fuga nostrum tempore aliquid cum. Doloribus, minima quis eius perferendis consequuntur quia perspiciatis odit.'
-      }
-    ],
-    created_at: String(Date.now()),
-    updated_at: String(Date.now())
-  })
+  const [user, setUser] = useState<IUser>(userTemplate)
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
