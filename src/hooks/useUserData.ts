cb20d3286c5b5c584eda23bf511dfc47227d0f7d
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-18 19:05:03 -0300

    remove unused code

--- src/hooks/useUserData.ts
+++
@@ -1,4 +0,0 @@
-import { UserDataContext } from 'contexts/UserDataContext'
-import { useContext } from 'react'
-
-export const useUserData = () => useContext(UserDataContext)
--- src/utils/calcReadingTime.ts
+++
@@ -1,5 +0,0 @@
-export const calcReadingTime = (text: string) => {
-  const totalWords = text.split('').length
-
-  return Math.round(totalWords / 200)
-}
