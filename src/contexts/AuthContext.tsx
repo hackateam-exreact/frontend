58f8bf063840ef190a930a2e4a47aec7e4eb3ad6
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-18 10:27:13 -0300

    add interface specific for signup fn and fix signup logic

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -11,9 +11,15 @@ import { IUser } from 'interfaces/user'
 import Router from 'next/router'
 import { api } from 'services/api'
 import { updateAuthCookies } from 'utils/updateAuthCookies'
-import { userTemplate } from 'utils/userTemplate'
 
 interface SignInParams {
+  email: string
+  password: string
+}
+
+interface SignUpParams {
+  first_name: string
+  last_name: string
   email: string
   password: string
 }
@@ -22,7 +28,7 @@ interface AuthContextData {
   user: IUser
   setUser: Dispatch<SetStateAction<IUser>>
   isAuthenticated: boolean
-  handleSignUp: (values: Partial<IUser>) => Promise<void>
+  handleSignUp: (values: SignUpParams) => Promise<void>
   handleSignIn: (values: SignInParams) => Promise<void>
 }
 
@@ -33,24 +39,23 @@ interface AuthProviderProps {
 export const AuthContext = createContext({} as AuthContextData)
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const [user, setUser] = useState<IUser>(userTemplate)
+  const [user, setUser] = useState<IUser>({} as IUser)
   const [isAuthenticated, setIsAuthenticated] = useState(!!user)
 
-  const handleSignUp = async (values: Partial<IUser>) => {
-    const { data } = await api.post('/api/users', values)
+  const handleSignUp = async (values: SignUpParams) => {
+    const { data: userData } = await api.post('/api/users', values)
 
     setUser({
-      ...data.user,
-      name: `${data.user.first_name} ${data.user.last_name}`,
-      avatar: '/img/chakra-logo.png',
-      password: values.password
+      ...userData.user,
+      name: `${userData.user.first_name} ${userData.user.last_name}`,
+      avatar: '/img/chakra-logo.png'
     })
+
+    await handleSignIn({ email: values.email, password: values.password })
   }
 
   const handleSignIn = async (values: SignInParams) => {
     const { data } = await api.post('/api/users/sign_in', values)
-
-    user.password = ''
 
     updateAuthCookies(data.token)
 
