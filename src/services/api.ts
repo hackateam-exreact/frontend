666d45b52a634be26e1a92f46a809768208e2b74
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-18 10:27:34 -0300

    remove unused variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,5 @@
 import axios from 'axios'
 
-const API_URL =
-  process.env.NODE_ENV === 'production'
-    ? process.env.NEXT_PUBLIC_API_URL
-    : 'http://localhost:4000'
-
 export const api = axios.create({
-  baseURL: API_URL
+  baseURL: process.env.NEXT_PUBLIC_API_URL
 })
