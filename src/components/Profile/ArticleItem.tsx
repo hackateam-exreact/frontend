99890dc2bd47295a702245814d82fbd862e3f52c
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-18 19:06:08 -0300

    fix user info origin

--- src/components/Profile/ArticleItem.tsx
+++ src/components/Profile/ArticleItem.tsx
@@ -1,33 +1,23 @@
-import { Divider, Flex, HStack, Icon, Text } from '@chakra-ui/react'
+import { Divider, Flex, Text } from '@chakra-ui/react'
 
-import { FiClock } from 'react-icons/fi'
 import { IArticle } from 'interfaces/article'
+import { IUser } from 'interfaces/user'
 import { ProfileSectionItemContainer } from 'components/Container/ProfileSectionItemContainer'
 import { ProfileSectionItemHeading } from 'components/Heading/ProfileSectionItemHeading'
 import { UserBadge } from './UserBadge'
-import { useAuth } from 'hooks/useAuth'
 
 interface ArticleItemProps {
+  profile: IUser
   article: IArticle
 }
 
-export function ArticleItem({ article }: ArticleItemProps) {
-  const { user } = useAuth()
-
+export function ArticleItem({ profile, article }: ArticleItemProps) {
   return (
     <ProfileSectionItemContainer>
       <ProfileSectionItemHeading>{article.title}</ProfileSectionItemHeading>
       <Divider />
       <Flex align="center" justify="space-between" w="100%">
-        <UserBadge user={user} />
-        <HStack>
-          <Icon as={FiClock} />
-          <Text fontSize="xs" color="gray.500">
-            {`${article.readingTime} ${
-              article.readingTime === 1 ? 'minuto' : 'minutos'
-            }`}
-          </Text>
-        </HStack>
+        <UserBadge user={profile} />
         <Text fontSize="xs" color="gray.500">
           {article.created_at}
         </Text>
