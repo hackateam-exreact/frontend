c10ce907421c900651bb2ff603c4d679943b45fc
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-18 14:42:36 -0300

    remove unused prop

--- src/components/Profile/ArticleList.tsx
+++ src/components/Profile/ArticleList.tsx
@@ -1,18 +1,16 @@
 import { ArticleItem } from './ArticleItem'
 import { IArticle } from 'interfaces/article'
-import { IUser } from 'interfaces/user'
 import { ProfileSectionContainer } from 'components/Container/ProfileSectionContainer'
 
 interface ArticleListProps {
-  user: IUser
   articles: IArticle[]
 }
 
-export function ArticleList({ user, articles }: ArticleListProps) {
+export function ArticleList({ articles }: ArticleListProps) {
   return (
     <ProfileSectionContainer title="Artigos">
-      {articles.map((article) => (
-        <ArticleItem key={article.id} user={user} article={article} />
+      {articles?.map((article) => (
+        <ArticleItem key={article.id} article={article} />
       ))}
     </ProfileSectionContainer>
   )
