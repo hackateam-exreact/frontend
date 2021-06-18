d2b44f62b3329a857cfa5c8cfcdc908d8734f617
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-18 14:46:16 -0300

    remove optional operator

--- src/interfaces/user.d.ts
+++ src/interfaces/user.d.ts
@@ -1,12 +1,12 @@
 export interface IUser {
   id: string
+  avatar: string | null
   first_name: string
   last_name: string
   email: string
-  location?: string | null
-  contact?: string | null
-  status?: 'Open' | 'Studying' | 'Employed' | null
-  about?: string | null
+  location: string | null
+  contact: string | null
+  status: 'Open' | 'Studying' | 'Employed' | null
+  about: string | null
   name: string | null
-  avatar?: string | null
 }
