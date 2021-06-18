87519c0ee101e6e044b7037f214b67063eb9ed04
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-18 11:23:17 -0300

    fix height overflow and made some changes to make responsive layout easier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,120 +1,105 @@
-import { Flex, Button, Text, Icon, Image } from '@chakra-ui/react'
 import { FiCode, FiUser } from 'react-icons/fi'
+import {
+  Flex,
+  HStack,
+  Icon,
+  Image,
+  SimpleGrid,
+  Text,
+  VStack
+} from '@chakra-ui/react'
+
+import { Button } from 'components/Button'
 import Router from 'next/router'
 
 export default function Home() {
+  const buttonProps = {
+    fontSize: '1rem',
+    fontWeight: '400',
+    fontFamily: 'Ubuntu',
+    marginLeft: '1rem',
+    w: '52',
+    py: '4rem'
+  }
+
+  const bannerSize = '550px'
+
   return (
     <>
       <Flex
         w="100%"
         maxW="1400px"
-        m="0 auto"
-        p="2rem"
-        my="2rem"
+        mx="auto"
+        p="8"
         height="70vh"
         alignItems="center"
         justifyContent="center"
       >
-        <Flex
-          w="50%"
-          flexDirection="column"
-          alignItems="flex-start"
-          justifyContent="center"
-          h="100%"
-        >
+        <SimpleGrid columns={2} w="100%">
+          <VStack align="flex-start" spacing="20">
+            <Image src="/img/logo.png" alt="Logo Devstop" h="64px" w="270px" />
+            <VStack align="flex-start">
+              <Text fontFamily="Ubuntu" fontSize="3rem">
+                Encontre outros{' '}
+                <Text as="span" color="blue.500">
+                  devs
+                </Text>
+                <br /> de todo o Brasil e <br />
+                ajude a{' '}
+                <Text as="span" color="green.500">
+                  comunidade
+                </Text>
+              </Text>
+              <Text
+                fontSize="1.5rem"
+                fontFamily="Roboto"
+                fontWeight="400"
+                lineHeight="29.5px"
+              >
+                Conheça desenvolvedores que estão <br />
+                no mercado e produza conteúdo em <br />
+                apenas uma plataforma <br />
+              </Text>
+            </VStack>
+          </VStack>
           <Image
-            src="/img/logo.png"
-            alt="Logo Devstop"
-            h="64px"
-            w="270px"
-            mb="5rem"
+            w={bannerSize}
+            h={bannerSize}
+            src="/img/banner.svg"
+            alt="Banner Img Devspot"
+            ml="auto"
           />
-
-          <Flex flexDirection="column">
-            <Text my="1rem" fontFamily="Ubuntu" fontSize="3rem">
-              Encontre outros{' '}
-              <Text as="span" color="blue.500">
-                devs
-              </Text>
-              <br /> de todo o Brasil e <br />
-              ajude a{' '}
-              <Text as="span" color="green.500">
-                comunidade
-              </Text>
-            </Text>
-            <Text
-              fontSize="1.5rem"
-              fontFamily="Roboto"
-              fontWeight="400"
-              lineHeight="29.5px"
-            >
-              Conheça desenvolvedores que estão <br />
-              no mercado e produza conteúdo em <br />
-              apenas uma plataforma <br />
-            </Text>
-          </Flex>
-        </Flex>
-        <Image
-          w="50%"
-          h="100%"
-          src="/img/banner.svg"
-          alt="Banner Img Devspot"
-        />
+        </SimpleGrid>
       </Flex>
-      <Flex height="30vh" bg="gray.800">
-        <Flex
-          w="100%"
-          maxW="1400px"
-          m="1rem auto"
-          p="2rem"
-          alignItems="center"
-          justifyContent="space-between"
-        >
-          <Flex flexDirection="column">
+      <Flex h="30vh" bg="gray.800">
+        <Flex w="100%" maxW="1400px" mx="auto" alignItems="center">
+          <VStack align="flex-start">
             <Text fontSize="2rem" fontWeight="500" fontFamily="Ubuntu">
               O que você deseja fazer?
             </Text>
             <Text>
               Você seria um recrutador <br /> ou um desenvolvedor?
             </Text>
-          </Flex>
+          </VStack>
 
-          <Flex w="40%" justifyContent="space-around">
+          <HStack spacing="5" ml="auto">
             <Button
-              w="45%"
-              py="4rem"
-              bg="blue.500"
-              _hover={{ bg: '#506EF9' }}
+              backgroundColor="blue.500"
+              leftIcon={<Icon as={FiCode} fontSize="32" />}
               onClick={() => Router.push('/dev/signup')}
+              {...buttonProps}
             >
-              <Icon as={FiCode} fontSize="32" />
-              <Text
-                fontSize="1rem"
-                fontWeight="400"
-                fontFamily="Ubuntu"
-                marginLeft="1rem"
-              >
-                Desenvolvedor
-              </Text>
+              Desenvolvedor
             </Button>
             <Button
-              w="45%"
-              py="4rem"
-              bg="green.500"
-              _hover={{ bg: '#03D05F' }}
+              backgroundColor="green.500"
+              leftIcon={<Icon as={FiUser} fontSize="32" />}
               onClick={() => Router.push('/hunter/signup')}
+              {...buttonProps}
             >
-              <Icon as={FiUser} fontSize="32" />
-              <Text
-                fontSize="1rem"
-                fontWeight="400"
-                fontFamily="Ubuntu"
-                marginLeft="1rem"
-              >
-                Recrutador
-              </Text>
+              Recrutador
             </Button>
-          </Flex>
+          </HStack>
         </Flex>
       </Flex>
     </>
