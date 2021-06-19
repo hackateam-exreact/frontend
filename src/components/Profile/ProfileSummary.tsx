4e3412c35ac263c84f3d2fcd340c5060de427b5b
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-18 22:13:12 -0300

    move button and modal to inside of each component

--- src/components/Profile/ProfileSummary.tsx
+++ src/components/Profile/ProfileSummary.tsx
@@ -1,9 +1,14 @@
-import { Image, SkeletonCircle, Text, VStack } from '@chakra-ui/react'
+import { Icon, Image, SkeletonCircle, Text, VStack } from '@chakra-ui/react'
 
+import { Button } from 'components/Button'
+import { EditProfileModal } from 'components/Modal/EditProfileModal'
+import { FiEdit } from 'react-icons/fi'
 import { IArticle } from 'interfaces/article'
 import { IProject } from 'interfaces/project'
 import { IUser } from 'interfaces/user'
 import { ProfileSectionItemContainer } from 'components/Container/ProfileSectionItemContainer'
+import { Protected } from 'components/Protected'
+import { useEditProfile } from 'hooks/useEditProfile'
 import { useState } from 'react'
 
 interface ProfileSummaryProps {
@@ -17,59 +22,69 @@ export function ProfileSummary({
   articles,
   projects
 }: ProfileSummaryProps) {
+  const { disclosure } = useEditProfile()
   const [isLoading, setIsLoading] = useState(true)
   const profilePicSize = '150px'
 
   return (
-    <ProfileSectionItemContainer
-      mode="flex"
-      direction="column"
-      w="72"
-      pos="relative"
-      mt="75px"
-    >
-      <SkeletonCircle
-        w={profilePicSize}
-        h={profilePicSize}
-        isLoaded={!isLoading}
-        pos="absolute"
-        top="0"
-        left="50%"
-        transform="translate(-50%, -50%)"
+    <>
+      <ProfileSectionItemContainer
+        mode="flex"
+        direction="column"
+        w="72"
+        pos="relative"
+        mt="75px"
       >
-        <Image
-          src="/img/chakra-logo.png"
-          alt={user.first_name}
+        <SkeletonCircle
           w={profilePicSize}
           h={profilePicSize}
-          objectFit="cover"
-          onLoad={() => setIsLoading(false)}
-        />
-      </SkeletonCircle>
-      <VStack spacing="5" mt="75px">
-        <Text
-          fontWeight="bold"
-          fontSize="lg"
-        >{`${user.first_name} ${user.last_name}`}</Text>
-        <Text fontSize="sm" color="gray.500" align="center">
-          {user.about}
-        </Text>
-        <Text color="blue.500">{user.email}</Text>
-      </VStack>
-      <VStack align="flex-start" spacing="2" mt="5">
-        <Text>
-          Artigos:{' '}
-          <Text as="span" color="blue.500">
-            {articles.length}
+          isLoaded={!isLoading}
+          pos="absolute"
+          top="0"
+          left="50%"
+          transform="translate(-50%, -50%)"
+        >
+          <Image
+            src="/img/chakra-logo.png"
+            alt={user.first_name}
+            w={profilePicSize}
+            h={profilePicSize}
+            objectFit="cover"
+            onLoad={() => setIsLoading(false)}
+          />
+        </SkeletonCircle>
+        <VStack spacing="5" mt="75px">
+          <Text
+            fontWeight="bold"
+            fontSize="lg"
+          >{`${user.first_name} ${user.last_name}`}</Text>
+          <Text fontSize="sm" color="gray.500" align="center">
+            {user.about}
           </Text>
-        </Text>
-        <Text>
-          Projetos principais:{' '}
-          <Text as="span" color="blue.500">
-            {projects.length}
+          <Text color="blue.500">{user.email}</Text>
+        </VStack>
+        <VStack align="flex-start" spacing="2" mt="5">
+          <Text>
+            Artigos:{' '}
+            <Text as="span" color="blue.500">
+              {articles.length}
+            </Text>
           </Text>
-        </Text>
-      </VStack>
-    </ProfileSectionItemContainer>
+          <Text>
+            Projetos principais:{' '}
+            <Text as="span" color="blue.500">
+              {projects.length}
+            </Text>
+          </Text>
+        </VStack>
+      </ProfileSectionItemContainer>
+
+      <Protected>
+        <Button leftIcon={<Icon as={FiEdit} />} onClick={disclosure.onOpen}>
+          Editar perfil
+        </Button>
+        <EditProfileModal />
+      </Protected>
+    </>
   )
 }
