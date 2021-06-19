d6add98940aa16af7a5535f9b32536038f6026bb
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-19 19:37:10 -0300

    fix cookie path

--- src/utils/updateAuthCookies.ts
+++ src/utils/updateAuthCookies.ts
@@ -1,5 +1,7 @@
 import { setCookie } from 'nookies'
 
 export const updateAuthCookies = (token: string, ctx = undefined) => {
-  setCookie(ctx, 'devspot.token', token)
+  setCookie(ctx, 'devspot.token', token, {
+    path: '/'
+  })
 }
