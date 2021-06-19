4dfc2f70cd0963d37171c70e16e0dbcfaf71f95e
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-19 01:15:03 -0300

    add link to logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
-import { Flex, HStack, Image } from '@chakra-ui/react'
+import { Link as ChakraLink, Flex, HStack, Image } from '@chakra-ui/react'
 
+import Link from 'next/link'
 import { Searchbar } from './Searchbar'
 import { UserBadge } from 'components/Profile/UserBadge'
 import { useAuth } from 'hooks/useAuth'
@@ -18,12 +19,16 @@ export function Header() {
     showHeader && (
       <Flex w="100%" h="24" borderBottom="1px" borderColor="gray.800" mb="5">
         <HStack w="100%" maxW="1400px" mx="auto" align="center" spacing="10">
-          <Image
-            src="/img/logo.png"
-            alt="Devspot"
-            w="225px"
-            objectFit="cover"
-          />
+          <Link href="/" passHref={true}>
+            <ChakraLink>
+              <Image
+                src="/img/logo.png"
+                alt="Devspot"
+                w="225px"
+                objectFit="cover"
+              />
+            </ChakraLink>
+          </Link>
           <Searchbar />
           <UserBadge user={user} badgeSide="right" />
         </HStack>
--- src/components/Profile/ArticleList.tsx
+++ src/components/Profile/ArticleList.tsx
@@ -1,14 +1,8 @@
 import { ArticleItem } from './ArticleItem'
-import { Button } from 'components/Button'
-import { CreateArticleModal } from 'components/Modal/CreateArticleModal'
-import { FiEdit } from 'react-icons/fi'
 import { IArticle } from 'interfaces/article'
 import { IUser } from 'interfaces/user'
-import { Icon } from '@chakra-ui/react'
 import { ProfileSectionContainer } from 'components/Container/ProfileSectionContainer'
-import { Protected } from 'components/Protected'
 import React from 'react'
-import { useCreateArticle } from 'hooks/useCreateArticle'
 
 interface ArticleListProps {
   profile: IUser
@@ -16,22 +10,11 @@ interface ArticleListProps {
 }
 
 export function ArticleList({ profile, articles }: ArticleListProps) {
-  const { handleCreateArticle } = useCreateArticle()
-
   return (
-    <>
-      <ProfileSectionContainer title="Artigos">
-        {articles?.map((article) => (
-          <ArticleItem key={article.id} profile={profile} article={article} />
-        ))}
-      </ProfileSectionContainer>
-
-      <Protected>
-        <Button leftIcon={<Icon as={FiEdit} />} onClick={handleCreateArticle}>
-          Criar artigo
-        </Button>
-        <CreateArticleModal />
-      </Protected>
-    </>
+    <ProfileSectionContainer title="Artigos">
+      {articles?.map((article) => (
+        <ArticleItem key={article.id} profile={profile} article={article} />
+      ))}
+    </ProfileSectionContainer>
   )
 }
