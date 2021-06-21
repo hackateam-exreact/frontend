e5992c7867832b6e08e513cf597d95e94438256d
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-20 22:30:41 -0300

    fix auth token undefined

--- src/utils/updateAuthCookies.ts
+++ src/utils/updateAuthCookies.ts
@@ -1,7 +1,12 @@
+import { api } from 'services/api'
 import { setCookie } from 'nookies'
 
 export const updateAuthCookies = (token: string, ctx = undefined) => {
   setCookie(ctx, 'devspot.token', token, {
     path: '/'
   })
+
+  api.defaults.headers = {
+    Authorization: `Bearer ${token}`
+  }
 }
